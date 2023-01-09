da1670746eff940275b895e9fd250e103ae46dce
Fenil Savani <fenilsavani322@gmail.com>
2023-01-09 23:47:59 +0530

    commit

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -7,7 +7,7 @@ const errorController=require('./controllers/error');
 
 const adminRoutes = require('./routes/Admin.js');
 const BrandRoutes = require('./routes/Brand.js');
-const ConsignmentRoutes = require('./routes/Consignment.js');
+const ConsignmentRoutes = require('./routes/Consignments');
 const InfluenceRoutes = require('./routes/Influecer');
 const ManagerRoutes = require('./routes/Manager');
 
@@ -15,12 +15,20 @@ const ManagerRoutes = require('./routes/Manager');
 
 
 app.use(bodyparser.urlencoded({extended:false}));
-// app.use(bodyparser.json())
+app.use(bodyparser.json())
 
+//importing all routers.....
 app.use('/admin',adminRoutes)
+app.use('/brand',BrandRoutes)
+app.use('/consignment',ConsignmentRoutes)
+app.use('/influence',InfluenceRoutes)
+app.use('/manager',ManagerRoutes)
+
+
 app.use(errorController.error404);
 
 const PORT=8000
+
 app.listen(PORT,()=>{
 console.log("running on server" + PORT )
 })
--- ServerSide/routes/Brand.js
+++ ServerSide/routes/Brand.js
@@ -4,5 +4,10 @@ const router=express.Router();
 const BrandController = require('../controllers/BrandController');
 
 
+router.get('/',(req,res,next)=>{
+        res.send('<h1>hello from brand </h1>');
+
+});
+
 
 module.exports=router;
